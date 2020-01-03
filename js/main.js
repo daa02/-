de79274fdd794f40f750449b6f1f1f33342239a6
oweb-web <dhekwjd26@naver.com>
2020-01-03 11:37:31 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -29,11 +29,13 @@ $(function(){
       }
     });  
 
+    // Swiper
     var swiper = new Swiper('.swiper-container', {
         effect: 'coverflow',
         grabCursor: true,
         centeredSlides: true,
         slidesPerView: 'auto',
+        loop: true,
         coverflowEffect: {
           rotate: 50,
           stretch: 0,
